2d20fd6bf546715b0ddc7f0508b94d0887dc91ed
AH-Ratul <ahratul21@gmail.com>
2025-11-15 23:29:55 +0600

    feat: handle user creation functionality

--- components/auth/register/register-form.tsx
+++ components/auth/register/register-form.tsx
@@ -1,71 +1,150 @@
 "use client";
-import { Eye, EyeOff } from "lucide-react";
-import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Password from "@/components/ui/Password";
+import { createUser } from "@/src/api/query/auth.query";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+const registerSchema = z
+  .object({
+    name: z.string("Name required"),
+    email: z.email(),
+    password: z.string().min(6, {
+      message: "password must be at least 6 characters long",
+    }),
+    confirmPassword: z.string().min(6, {
+      message: "password must be at least 6 characters long",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password doesn't match",
+    path: ["confirmPassword"],
+  });
+
+type RegisterFormValues = z.infer<typeof registerSchema>;
 
 const RegisterForm = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+
+  const form = useForm<RegisterFormValues>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: createUser,
+    onSuccess: (res) => {
+      toast.success(res.message);
+      form.reset();
+      router.push("/login");
+    },
+    onError: (err: any) => {
+      toast.error(err.response.data.message);
+    },
+  });
+
+  const onSubmit = async (data: RegisterFormValues) => {
+    const userInfo = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
+
+    mutate(userInfo);
+  };
   return (
-    <form>
-      <div>
-        <label htmlFor="name" className="font-semibold text-sm">
-          Name
-        </label>
-
-        <input
-          id="name"
-          type="text"
-          placeholder="@: ahr"
-          className="w-full block border border-gray-200 focus:ring-1 focus:ring-primary focus:outline-none px-3 py-2 rounded-lg text-sm my-1 shadow-xs"
-        />
-      </div>
-
-      <div>
-        <label htmlFor="email" className="font-semibold text-sm">
-          Email
-        </label>
-
-        <input
-          id="email"
-          type="email"
-          placeholder="@: ahr@e.com"
-          className="w-full block border border-gray-200 focus:ring-1 focus:ring-primary focus:outline-none px-3 py-2 rounded-lg text-sm my-1 shadow-xs"
-        />
-      </div>
-
-      <div>
-        <label htmlFor="password" className="font-semibold text-sm">
-          Password
-        </label>
-
-        <div className="relative">
-          <input
-            id="password"
-            type={showPassword ? "text" : "password"}
-            placeholder="@: xxxxxxxx"
-            className="w-full block border border-gray-200 focus:ring-1 focus:ring-primary focus:outline-none px-3 py-2 rounded-lg text-sm my-1 shadow-xs"
-          />
-          <button
-            type="button"
-            onClick={() => setShowPassword((prev) => !prev)}
-            className="absolute right-0 inset-y-0 pr-3 cursor-pointer flex items-center outline-none"
-            aria-label={showPassword ? "Hide Password" : "Show Password"}
-          >
-            {showPassword ? (
-              <EyeOff height={20} width={20} />
-            ) : (
-              <Eye height={20} width={20} />
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-1">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-semibold text-sm">Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="@: ahr" {...field} />
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
             )}
-          </button>
-        </div>
-      </div>
+          />
 
-      <button
-        type="submit"
-        className="bg-primary text-white text-sm w-full rounded-lg py-1.5 mt-4 cursor-pointer hover:opacity-95 "
-      >
-        Register
-      </button>
-    </form>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-semibold text-sm">Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="@: ahr@e.com" type="email" {...field} />
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-semibold text-sm">
+                  Password
+                </FormLabel>
+                <FormControl>
+                  <Password {...field} />
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-semibold text-sm">
+                  Confirm Password
+                </FormLabel>
+                <FormControl>
+                  <Password {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full cursor-pointer mt-2">
+            {isPending ? "Proccessing..." : "Register"}
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
