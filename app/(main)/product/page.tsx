c697f6583f0be17b51a468c97f62fad0f6092112
AH-Ratul <ahratul21@gmail.com>
2025-11-17 18:55:28 +0600

    feat: product page added

---
+++ app/(main)/product/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Shared/header";
+import AllProductsTable from "./_ui/product_table";
+
+const ProductPage = () => {
+  return (
+    <>
+      <Header />
+      <AllProductsTable />
+    </>
+  );
+};
+
+export default ProductPage;
