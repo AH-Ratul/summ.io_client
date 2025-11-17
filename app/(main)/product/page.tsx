bf0f530f3dcfedd821e3e24ce8a3c9c023c4d309
AH-Ratul <ahratul21@gmail.com>
2025-11-17 19:54:18 +0600

    update: header removed

--- app/(main)/product/page.tsx
+++ app/(main)/product/page.tsx
@@ -1,10 +1,8 @@
-import Header from "@/components/Shared/header";
 import AllProductsTable from "./_ui/product_table";
 
 const ProductPage = () => {
   return (
     <>
-      <Header />
       <AllProductsTable />
     </>
   );
