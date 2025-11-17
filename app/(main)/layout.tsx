d9ad4066bc82fcd62b5f9b0336889a841707efc9
AH-Ratul <ahratul21@gmail.com>
2025-11-17 19:58:13 +0600

    update: fixed sidebar width, header position & main content scrollable

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,17 @@
 import { PropsWithChildren } from "react";
 import Sidebar from "./_ui/sidebar/Sidebar";
+import Header from "@/components/Shared/header";
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
-    <main className="flex w-full h-dvh">
-      <Sidebar className="min-w-64 hidden md:flex" />
+    <div className="flex h-dvh overflow-hidden">
+      <Sidebar className="min-w-64 hidden md:flex shrink-0" />
 
-      <section className="w-full h-dvh">{children}</section>
-    </main>
+      <section className="w-full h-dvh flex flex-col">
+        <Header />
+
+        <main className="flex-1 overflow-y-auto p-4">{children}</main>
+      </section>
+    </div>
   );
 }
