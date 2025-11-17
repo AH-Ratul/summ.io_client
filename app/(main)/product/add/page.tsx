489cfada31de7667d01cbdec02acbcbb469a40ec
AH-Ratul <ahratul21@gmail.com>
2025-11-17 19:55:01 +0600

    update: header removed

--- app/(main)/product/add/page.tsx
+++ app/(main)/product/add/page.tsx
@@ -1,11 +1,8 @@
-import Header from "@/components/Shared/header";
-import React from "react";
 import AddProduct from "./_ui/addProduct";
 
 const AddProductPage = () => {
   return (
     <div>
-      <Header />
       <AddProduct />
     </div>
   );
