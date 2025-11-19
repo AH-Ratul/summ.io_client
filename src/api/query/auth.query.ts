911476bb9ec289168c1419410ad4173023c04918
AH-Ratul <ahratul21@gmail.com>
2025-11-19 16:12:18 +0600

    handle logout api functionality

--- src/api/query/auth.query.ts
+++ src/api/query/auth.query.ts
@@ -5,3 +5,7 @@ export const createUser = async (payload: Object) => {
   const { data } = await axiosInstance.post(apiUrl.createUser, payload);
   return data;
 };
+
+export const logoutUser = () => {
+  return axiosInstance.post(apiUrl.logout);
+};
