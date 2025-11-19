621c6aabe5a95dd4ff7433bdbb122809d1a62350
AH-Ratul <ahratul21@gmail.com>
2025-11-19 16:08:44 +0600

    feat: handle product api performs

---
+++ src/api/query/product.query.ts
@@ -0,0 +1,16 @@
+import { axiosInstance } from "@/lib/axios";
+import { apiUrl } from "../api_url";
+
+export const addProduct = async (payload: Object) => {
+  const { data } = await axiosInstance.post(apiUrl.addProduct, payload);
+  return data;
+};
+
+export const getProduct = async () => {
+  const { data } = await axiosInstance.get(apiUrl.getProduct);
+  return data;
+};
+
+export const deleteProduct = async (id: string) => {
+  return await axiosInstance.delete(apiUrl.deleteProduct(id));
+};
