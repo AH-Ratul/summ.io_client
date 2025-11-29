8cfa78ab0348881812da44b5828ebd93e96a3041
AH-Ratul <ahratul21@gmail.com>
2025-11-29 21:56:21 +0600

    feat: manage allExpense components

---
+++ app/(main)/expense/_ui/all_expenses.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import AddExpense from "./add_expense";
+
+const AllExpenses = () => {
+  return (
+    <div className="px-4 lg:px-8 my-5 mx-auto max-w-7xl">
+      <AddExpense />
+    </div>
+  );
+};
+
+export default AllExpenses;
