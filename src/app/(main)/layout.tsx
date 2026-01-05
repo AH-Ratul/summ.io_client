95f07f8d339c2498265b85e4bf856fdcc8d0eaad
AH-Ratul <ahratul21@gmail.com>
2026-01-05 15:01:58 +0600

    feat: implement collapsible sidebar with shared modal state

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,18 @@
+"use client";
+
 import { PropsWithChildren } from "react";
 import Sidebar from "./_ui/sidebar/Sidebar";
 import Header from "@/src/components/Shared/header";
+import { useModalState } from "@/src/hooks/hook";
 
-export default async function Layout({ children }: PropsWithChildren) {
+export default function Layout({ children }: PropsWithChildren) {
+  const { open, onOpenChange } = useModalState(true);
   return (
     <div className="flex h-dvh overflow-hidden">
-      <Sidebar className="min-w-64 hidden md:flex shrink-0" />
+      {open && <Sidebar className="min-w-64 hidden md:flex shrink-0" />}
 
       <section className="w-full h-dvh flex flex-col">
-        <Header />
+        <Header open={open} onOpenChange={onOpenChange} />
 
         <main className="flex-1 overflow-y-auto">{children}</main>
       </section>
