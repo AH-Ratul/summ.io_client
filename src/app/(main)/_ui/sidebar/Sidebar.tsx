77acced87d728472c29017870ebc866bc406b525
AH-Ratul <ahratul21@gmail.com>
2026-01-05 12:06:53 +0600

    feat: implement dynamic rendering for sidebar links

--- src/app/(main)/_ui/sidebar/Sidebar.tsx
+++ src/app/(main)/_ui/sidebar/Sidebar.tsx
@@ -4,16 +4,21 @@ import { cn } from "@/src/lib/utils";
 
 import { SidebarLinks } from "./sidebar_links";
 import Logo from "../../../../../public/icons/Logo";
+import { useSidebarLinks } from "./use.sidebar_links";
 
 const Sidebar = ({ className }: { className?: string }) => {
+  const { sidebarLinks } = useSidebarLinks();
+
   return (
     <aside
       className={cn(`min-h-screen border-r flex flex-col py-4 px-4`, className)}
     >
       <Logo />
 
-      <section>
-        <SidebarLinks />
+      <section className="mt-10">
+        {sidebarLinks?.map((link, index) => (
+          <SidebarLinks key={index} {...link} />
+        ))}
       </section>
     </aside>
   );
