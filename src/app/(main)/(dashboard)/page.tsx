507319db9371a61363fea62d032589a5469ff67d
AH-Ratul <ahratul21@gmail.com>
2026-01-06 17:58:18 +0600

    feat: implement dashboard home page with summary cards

--- src/app/(main)/(dashboard)/page.tsx
+++ src/app/(main)/(dashboard)/page.tsx
@@ -1,8 +1,31 @@
+"use client";
 
-export default async function Home() {
+import { useQuery } from "@tanstack/react-query";
+import SummaryCard from "./_ui/summary";
+import { getDashboardData } from "@/src/api/query/dashboard.query";
+import { CONST } from "@/src/lib/const";
+
+export default function Home() {
+  const { data: metaData, isLoading } = useQuery({
+    queryKey: ["META"],
+    queryFn: getDashboardData,
+  });
+
+  const revenue = metaData?.data?.totalRevenue?._sum?.total;
+  const expense = metaData?.data?.totalExpense?._sum?.amount;
+  const pr = metaData?.data?.totalProduct;
+
+  if (isLoading) {
+    return "loading";
+  }
+
   return (
-    <div>
-      <p>Dashbaord</p>
+    <div className="px-4 sm:px-8 my-5 mx-auto max-w-7xl">
+      <div className="flex flex-col md:flex-row items-center gap-3 md:gap-5">
+        <SummaryCard title={"Revenue"} value={revenue} icon={CONST.TK} />
+        <SummaryCard title={"Total Expense"} value={expense} icon={CONST.TK} />
+        <SummaryCard title={"Total Product"} value={pr} />
+      </div>
     </div>
   );
 }
