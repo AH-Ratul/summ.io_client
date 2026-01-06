3243cbc1be090ac8ed0d0aa1013f5c6dcd94d301
AH-Ratul <ahratul21@gmail.com>
2026-01-06 17:52:37 +0600

    update: responsive breakpoint

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: PropsWithChildren) {
   const { open, onOpenChange } = useModalState(true);
   return (
     <div className="flex h-dvh overflow-hidden">
-      {open && <Sidebar className="min-w-64 hidden md:flex shrink-0" />}
+      {open && <Sidebar className="min-w-64 hidden xl:flex shrink-0" />}
 
       <section className="w-full h-dvh flex flex-col">
         <Header open={open} onOpenChange={onOpenChange} />
