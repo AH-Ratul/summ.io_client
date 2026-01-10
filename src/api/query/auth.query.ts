44f132f91db15b6437c52258f6a383bfe23fc440
AH-Ratul <ahratul21@gmail.com>
2026-01-10 15:38:51 +0600

    feat: implement login credentiality using axios

--- src/api/query/auth.query.ts
+++ src/api/query/auth.query.ts
@@ -6,6 +6,11 @@ export const createUser = async (payload: Object) => {
   return data;
 };
 
+export const credentialsLogin = async (payload: Object) => {
+  const { data } = await axiosInstance.post(apiUrl.credentialsLogin, payload);
+  return data;
+};
+
 export const logoutUser = () => {
   return axiosInstance.post(apiUrl.logout);
 };
