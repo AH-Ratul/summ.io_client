d259907a27fde37f982d9997a527010456d94a3f
AH-Ratul <ahratul21@gmail.com>
2026-01-10 15:35:06 +0600

    deleted

--- src/helpers/getUserSession.ts
+++
@@ -1,5 +0,0 @@
-"use server";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./authOptions";
-
-export const getUserSession = async () => await getServerSession(authOptions);
