6fdb76809b0f41c8710bb40574410bfb47932c27
AH-Ratul <ahratul21@gmail.com>
2026-01-10 15:37:05 +0600

    define rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,14 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-wrapper-object-types": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-wrapper-object-types": "warn",
+      "react/no-unescaped-entities": "warn"
+    }
+  }
 ]);
 
 export default eslintConfig;
