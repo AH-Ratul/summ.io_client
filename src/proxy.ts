3f1a2e41d0e66c5922ed714d177964bba61935d2
AH-Ratul <ahratul21@gmail.com>
2026-01-10 15:33:00 +0600

    refactor: route configuration

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,16 @@
-export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
+
+export default function proxy(req: NextRequest) {
+  const token = req.cookies.get("accessToken")?.value;
+
+  const loginURL = new URL("/login", req.url);
+
+  if (!token) {
+    return NextResponse.redirect(loginURL);
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/", "/product/:path*", "/sales/:path*", "/expense/:path*"],
